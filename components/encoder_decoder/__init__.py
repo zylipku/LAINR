e863e39dde1f88507fabd442e39d1806dcb66630
zylipku <123136644+zylipku@users.noreply.github.com>
2023-09-26 15:51:18 +0800

    remove debugging files

--- components/encoder_decoder/__init__.py
+++ components/encoder_decoder/__init__.py
@@ -1,12 +1,5 @@
 from .abstract_ed import EncoderDecoder
 
-from .pca import PCAED
 from .cae import CAEED
-from .cae400 import CAE400ED
 from .aeflow import AEflowED
-from .fouriernet import FourierNetED
-from .fouriernet2 import INR2ED
 from .sinr import SINRED
-from .sinr2 import SINR2ED
-from .sinr3 import SINR3ED
-from .sinr4 import SINR4ED

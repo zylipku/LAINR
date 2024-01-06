9170a9a3296258fd28f94bea1a8627e85e7e383d
LiAlH4 <j.lizy@icloud.com>
2024-01-06 11:16:55 +0800

    ablation for cae

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k3 nepochs=20000
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k5 nepochs=20000
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k7 nepochs=20000
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k3 nepochs=20000
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k5 nepochs=20000
-python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k7 nepochs=20000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k3 nepochs=30000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k5 nepochs=30000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h16_k7 nepochs=30000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k3 nepochs=30000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k5 nepochs=30000
+python main.py --phase=pretrain encoder_decoder=ablation/cae/h32_k7 nepochs=30000

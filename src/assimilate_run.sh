9734f840504a868d700047f6cda716d88a616c59
LiAlH4 <j.lizy@icloud.com>
2024-01-10 22:19:23 +0800

    update assimilate heatmap

--- src/assimilate_run.sh
+++ src/assimilate_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.1,0.03,0.01,0.003,0.001 \
-encoder_decoder=ablation/cae/h32_k5 latent_dynamics=default_rezero
-python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.1,0.03,0.01,0.003,0.001 \
-encoder_decoder=ablation/aeflow/b4_k5 latent_dynamics=default_rezero
-python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.01,0.003,0.001,0.0003,0.0001 \
+# python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.1,0.03,0.01,0.003,0.001 \
+# encoder_decoder=ablation/cae/h32_k5 latent_dynamics=default_rezero
+# python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.1,0.03,0.01,0.003,0.001 \
+# encoder_decoder=ablation/aeflow/b4_k5 latent_dynamics=default_rezero
+python assimilate_main.py --multirun sigma_z_b=0.01,0.001 sigma_m=0.1,0.03 \
 encoder_decoder=default_sinr_v11 latent_dynamics=default_neuralode

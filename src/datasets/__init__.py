198727d2b537d19b65b14e2614a035ee40f4ee82
LiAlH4 <j.lizy@icloud.com>
2024-01-15 13:54:32 +0800

    add era5v00

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,6 +4,7 @@ from .la_dataset import MetaData
 
 from .sw import ShallowWater
 from .era5 import ERA5
+from .era5v00 import ERA5v00
 from .era5v01 import ERA5v01
 from .era5v02 import ERA5v02
 
@@ -16,11 +17,18 @@ def load_dataset(logger: logging.Logger, cfg: DatasetConfig, **kwargs):
 
     if ds_name == 'sw':
         return ShallowWater(logger=logger, cfg=cfg, **kwargs)  # alias with DINoShallowWater3
+    
     elif ds_name == 'era5':
         return ERA5(logger=logger, cfg=cfg, **kwargs)
+    
+    elif ds_name == 'era5v00':
+        return ERA5v00(logger=logger, cfg=cfg, **kwargs)
+    
     elif ds_name == 'era5v01':
         return ERA5v01(logger=logger, cfg=cfg, **kwargs)
+    
     elif ds_name == 'era5v02':
         return ERA5v02(logger=logger, cfg=cfg, **kwargs)
+    
     else:
         raise ValueError(f"dataset_name '{ds_name}' not supported")

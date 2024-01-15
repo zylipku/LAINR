73bb840cd4e50075add815b1a56b72caa0a864d3
LiAlH4 <j.lizy@icloud.com>
2024-01-15 12:22:25 +0800

    add era5v02,3 pretrain

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -6,6 +6,7 @@ from .sw import ShallowWater
 from .era5 import ERA5
 from .era5v01 import ERA5v01
 from .era5v02 import ERA5v02
+from .era5v03 import ERA5v03
 
 from configs.conf_schema import DatasetConfig
 
@@ -22,5 +23,7 @@ def load_dataset(logger: logging.Logger, cfg: DatasetConfig, **kwargs):
         return ERA5v01(logger=logger, cfg=cfg, **kwargs)
     elif ds_name == 'era5v02':
         return ERA5v02(logger=logger, cfg=cfg, **kwargs)
+    elif ds_name == 'era5v03':
+        return ERA5v03(logger=logger, cfg=cfg, **kwargs)
     else:
         raise ValueError(f"dataset_name '{ds_name}' not supported")

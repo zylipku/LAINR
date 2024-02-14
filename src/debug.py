938e9c70e992a9ce8edd4c4079129a99dc80a496
LiAlH4 <j.lizy@icloud.com>
2024-02-14 21:09:41 +0800

    remove unnecessary files

--- src/debug.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-from modules import AEflowV2 as AEflow
-x = torch.randn(3, 2, 128, 64)
-model = AEflow(state_channels=2, kernel_size=3)
-y = model(x)
-print(f'{y.shape=}')
-torch.nn.functional.conv_transpose2d()
